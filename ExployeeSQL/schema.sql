2929475ef5ae055752cf142d75e030192a24ab75
Andrew Miller <amiller9108@gmail.com>
2021-05-12 14:39:42 -0400

    finished sql queries

--- ExployeeSQL/schema.sql
+++ ExployeeSQL/schema.sql
@@ -1,45 +1,79 @@
-CREATE TABLE Departments (
-	dept_no INTEGER	NOT NULL,
-	dept_name VARCHAR	NOT NULL,
-	PRIMARY KEY (dept_no)
+﻿-- Table Schemata and Data Engineering
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL
 );
 
-CREATE TABLE Department_Employees (
-	emp_no INTEGER	NOT NULL,
-	dept_no INTEGER	NOT NULL,
-	PRIMARY KEY (emp_no),
-	FOREIGN KEY(dept_no) REFERENCES Departments (dept_no)
+CREATE TABLE "department_employees" (
+    "emp_no" INTEGER   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL
 );
 
-CREATE TABLE Department_Manager (
-	dept_no INTEGER	NOT NULL,
-	emp_no INTEGER	NOT NULL,
-	FOREIGN KEY(dept_no) REFERENCES Departments (dept_no),
-	FOREIGN KEY(emp_no) REFERENCES Department_Employees (emp_no)
+CREATE TABLE "department_manager" (
+    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" INTEGER   NOT NULL
 );
 
-CREATE TABLE Employees (
-	emp_no INTEGER	NOT NULL,
-	emp_title_id VARCHAR	NOT NULL,
-	birth_date DATE	NOT NULL,
-	first_name VARCHAR	NOT NULL,
-	last_name VARCHAR	NOT NULL,
-	sex VARCHAR	NOT NULL,
-	hire_date DATE,
-	PRIMARY KEY (emp_title_id),
-	FOREIGN KEY(emp_no) REFERENCES Department_Employees (emp_no)
+CREATE TABLE "employees" (
+    "emp_no" INTEGER   NOT NULL,
+    "emp_title_id" VARCHAR   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL
 );
 
-CREATE TABLE Salaries (
-	emp_no INTEGER	NOT NULL,
-	salary INTEGER	NOT NULL,
-	PRIMARY KEY (salary),
-	FOREIGN KEY(emp_no) REFERENCES Department_Employees (emp_no)
+CREATE TABLE "salaries" (
+    "emp_no" INTEGER   NOT NULL,
+    "salary" INTEGER   NOT NULL
 );
 
-CREATE TABLE Titles (
-	title_id VARCHAR	NOT NULL,
-	title VARCHAR	NOT NULL,
-	PRIMARY KEY (title),
-	FOREIGN KEY (title_id) REFERENCES Employees (emp_title_id)
+CREATE TABLE "titles" (
+    "title_id" VARCHAR   NOT NULL,
+    "title" VARCHAR   NOT NULL
 );
+
+
+-- Import CSV files into corresponding tables 
+-- Alter tables to form relationships
+
+ALTER TABLE "titles"
+ADD "emp_no" INTEGER;
+
+ALTER TABLE "departments" 
+ADD CONSTRAINT "pk_departments" PRIMARY KEY ("dept_no");
+
+ALTER TABLE "employees" 
+ADD CONSTRAINT "pk_employees" PRIMARY KEY ("emp_no");
+
+
+
+ALTER TABLE "department_employees" ADD CONSTRAINT "fk_department_employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "department_employees" ADD CONSTRAINT "fk_department_employees_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "department_manager" ADD CONSTRAINT "fk_department_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "department_manager" ADD CONSTRAINT "fk_department_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+-- Query * from tables to confirm data
+SELECT * FROM departments;
+SELECT * FROM department_employees;
+SELECT * FROM department_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
+
+
